e5dafd76c58041afdf00482b0a603856d00ea69c
Doge Gui <guiqiqi187@gmail.com>
2024-05-01 01:38:26 +0300

    Basic Application layout

--- src/main/java/webtoy/Application.java
+++ src/main/java/webtoy/Application.java
@@ -2,15 +2,45 @@ package webtoy;
 
 import java.util.List;
 
-public abstract class Application {
+public class Application {
 
-    public abstract Response respond(Request request);
+    private Router router;
 
-    public abstract void route(String path, HTTPMethod method, Handler handler);
+    /**
+     * Respond to TCP server with request.
+     * 
+     * If no suitable method found but some other path matched, return an HTTP 405 response;
+     * If no path matched at all, return an HTTP 404 response;
+     * If any Exception catched during handler's applying, return an HTTP 502 response.
+     * 
+     * @param request parsed from TCP server
+     * @return generated response
+     */
+    public Response respond(Request request) {
+        try {
+            Handler handler = this.router.match(request.path, request.method);
+            return handler.apply(request);
+        } catch (Router.UnsupportedMethod error) {
+            return new Response(405);
+        } catch (Router.URLNotFound error) {
+            return new Response(404);
+        } catch (Exception error) {
+            return new Response(502);
+        }
+    }
+
+    /**
+     * Register handler with given path and methods.
+     * @param path of request
+     * @param method bound to given url path
+     * @param handler for generating response
+     */
+    public void route(String path, HTTPMethod method, Handler handler) {
+        this.router.register(path, List.of(method), handler);
+    }
 
     public void route(String path, List<HTTPMethod> methods, Handler handler) {
-        for (HTTPMethod method : methods)
-            this.route(path, method, handler);
+        this.router.register(path, methods, handler);
     }
 
     public void route(String path, Handler handler) {
